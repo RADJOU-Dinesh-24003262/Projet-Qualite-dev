70dfd1da5a08f7ce90254f2e46cf0148b6b58dec
AmirTahaChaoui <amir.TAHA-CHAOUI@etu.univ-amu.fr>
2025-11-27 08:51:02 +0100

    Ajout méthode makePotion() dans Druid

--- src/main/java/org/example/Model/Character/Gallic/Druid.java
+++ src/main/java/org/example/Model/Character/Gallic/Druid.java
@@ -1,4 +1,20 @@
 package org.example.Model.Character.Gallic;
 
+import org.example.Model.Potion.Potion;
+
 public class Druid extends Gallic {
+
+    public Druid() {
+        super();
+    }
+
+    public Druid(String name) {
+        super();
+        this.setName(name);
+    }
+
+    public Potion makePotion() {
+        System.out.println(getName() + " prépare une potion magique...");
+        return new Potion();
+    }
 }
