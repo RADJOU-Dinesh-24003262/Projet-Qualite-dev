85cbf55c1ef70bdf89b677ad1c7a2c618d5129a2
rombiMatis <matis.rombi@etu.univ-amu.fr>
2025-11-28 10:03:40 +0100

    Improvement of the addCharacter method

--- src/Places/AbstractPlace.java
+++ src/Places/AbstractPlace.java
@@ -2,6 +2,8 @@ package Places;
 
 import java.util.ArrayList;
 import Character.AbstractCharacter;
+import Character.Roman.General;
+import Character.Roman.RomanLegionary;
 import Food.AbstractFood;
 import Character.Werewolves;
 import Character.Gallic.Gallic;
@@ -28,10 +30,61 @@ public abstract class AbstractPlace{
     }
 
     public void addCharacter(AbstractCharacter character) {
-        presentCharacters.add(character);
         if (this.type == TypePlace.gallicVillage) {
-            if (character instanceof Gallic || character instanceof Werewolves) {
+            if (character == null) {
+                throw new IllegalArgumentException("La liste contient un personnage null");
+            } else if (character instanceof Gallic || character instanceof Werewolves) {
+                presentCharacters.add(character);
+            } else {
+                throw new IllegalArgumentException("Le personnage " + character.getClass().getName() + "ne peux pas être présent dans un village gaulois");
+            }
+        }
 
+        if (this.type == TypePlace.romanFortifiedCamp) {
+            if (character == null) {
+                throw new IllegalArgumentException("La liste contient un personnage null");
+            } else if (character instanceof RomanLegionary || character instanceof General || character instanceof Werewolves) {
+                presentCharacters.add(character);
+            } else {
+                throw new IllegalArgumentException("Le personnage " + character.getClass().getName() + "ne peux pas être présent dans un camp retranché romain");
+            }
+        }
+
+        if (this.type == TypePlace.romanCity) {
+            if (character == null) {
+                throw new IllegalArgumentException("La liste contient un personnage null");
+            } else if (character instanceof Roman || character instanceof Werewolves) {
+                presentCharacters.add(character);
+            } else {
+                throw new IllegalArgumentException("Le personnage " + character.getClass().getName() + "ne peux pas être présent dans une ville romaine");
+            }
+        }
+
+        if (this.type == TypePlace.galloRomanVillage) {
+            if (character == null) {
+                throw new IllegalArgumentException("La liste contient un personnage null");
+            } else if (character instanceof Roman || character instanceof Gallic) {
+                presentCharacters.add(character);
+            } else {
+                throw new IllegalArgumentException("Le personnage " + character.getClass().getName() + "ne peux pas être présent dans une bourgade gallo-romaine");
+            }
+        }
+
+        if (this.type == TypePlace.enclosure) {
+            if (character == null) {
+                throw new IllegalArgumentException("La liste contient un personnage null");
+            } else if (character instanceof Werewolves) {
+                presentCharacters.add(character);
+            } else {
+                throw new IllegalArgumentException("Le personnage " + character.getClass().getName() + "ne peux pas être présent dans un camp retranché romain");
+            }
+        }
+
+        if (this.type == TypePlace.battlefield) {
+            if (character == null) {
+                throw new IllegalArgumentException("La liste contient un personnage null");
+            } else {
+                presentCharacters.add(character);
             }
         }
     }
